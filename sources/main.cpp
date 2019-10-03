f99ad0472a7c8eada6fb227f9669ca53669bcb56
YuriySavchenko <yurec.proger@gmail.com>
2019-10-03 12:04:04 +0300

    implementation of the main.cpp file

---
+++ sources/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ctime>
+
+#include "datamanager.h"
+
+int main(int argc, char *argv[]) {
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    std::unique_ptr<DataManager<int>> manager(new DataManager<int>(3));
+
+    manager->execute();
+
+    return 0;
+}
