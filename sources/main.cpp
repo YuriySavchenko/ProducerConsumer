531c9dee065d3943197dcfd5b0895f62cda0cc67
yurii.savchenko <yurii.savchenko@globallogic.com>
2019-10-07 21:43:17 +0300

    increased count of the consumers

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <ctime>
-
 #include "datamanager.h"
 
 int main(int argc, char *argv[]) {
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    std::unique_ptr<DataManager<int>> manager(new DataManager<int>(3));
+    std::unique_ptr<DataManager<int>> manager(new DataManager<int>(5));
 
     manager->execute();
 
